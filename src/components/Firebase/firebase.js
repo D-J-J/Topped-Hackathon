f2d26b082dbc55bcaffbeffcef7b71a40e6dd0fd
Henry <wjxhenry@gmail.com>
2020-01-19 01:43:49 -0700

    Basic adding

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from "firebase/app";
+import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
@@ -8,13 +9,16 @@ const firebaseConfig = {
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  };
+};
 
-  class Firebase {
+class Firebase {
     constructor() {
         app.initializeApp(firebaseConfig);
 
         this.auth = app.auth();
+        this.firestore = app.firestore();
+
+        this.doInsertCompetition("newComp", "data2", 2);
     }
 
     doCreateUserWithEmailAndPassword = (email, password) =>
@@ -29,6 +33,23 @@ const firebaseConfig = {
 
     doPasswordUpdate = password =>
         this.auth.currentUser.updatePassword(password);
+
+    /**
+     * @param path The path: collection/path/collection/path/...
+     */
+    doInsertCompetition = (name, data1, data2) => {
+        let competitionRef = this.firestore.collection("competitions").doc();
+        competitionRef.set({
+            id: competitionRef.id,
+            test1: "Test1",
+            test2: "Test2"
+        });
+        console.log(competitionRef.id)
+        // this.firestore.doc('competitions/' + name).set({
+        //     data1: data1,
+        //     data2: data2
+        // });
+    }
 }
 
   
