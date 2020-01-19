41a365f87b450ee717450b670d01a9db005766a8
darrentran <dtran1@ualberta.ca>
2020-01-19 05:07:13 -0700

    removed unused import

--- src/components/Competition/CompetitionProblemCard.js
+++ src/components/Competition/CompetitionProblemCard.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
 import { IconButton } from '@material-ui/core';
