4702d76e952765939a698013faa0331e11dd824b
jameshahaha <jameshahaha@icloud.com>
2020-01-19 01:05:55 -0700

    minor changes

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 const Landing = () => (
     <div>
         <h1>Competitions</h1>
--- src/models/competition.js
+++ src/models/competition.js
@@ -1,18 +1,8 @@
 import React from 'react';
 var currentDate = new Date().toJSON();
-console.log(currentDate);
-
 var statusEnum = Object.freeze({"active":1, "completed":2, "not_started":3})
 
 export default class Competition {
-    // id = 0;
-    // name = "";
-    // desc = "";
-    // startDate = new Date(year, month, day, hours, minutes, seconds, milliseconds);
-    // endDate = new Date(year, month, day, hours, minutes, seconds, milliseconds);
-    // problems = 0;
-    // fee = 0;
-    
 
     constructor(name, desc, startDate, endDate, problems, fee, admins) {
         this.id; // generate
