ad0a6d0922e4b5e4fc089553bacdfbe321ee91b3
Henry <wjxhenry@gmail.com>
2020-01-19 08:16:46 -0700

    Connect stash list

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
+import { withFirebase } from "../Firebase";
 // import CompetitionProblemCard from "../Competition/CompetitionProblemCard";
+import { compose } from "recompose";
 import CompetitionCard from "../Competition/CompetitionCard";
 
 // let problems = [];
@@ -7,46 +9,78 @@ import CompetitionCard from "../Competition/CompetitionCard";
 //     problems.push({ problem_number: i, attempts_made: 1, completed: false });
 // }
 
-let competitions = [];
-for (var i = 0; i < 2; i++) {
-    competitions.push({
-        id: i,
-        name: "competition " + i,
-        desc: "description " + i,
-        startDate: new Date(),
-        endDate: new Date(),
-        duration: 1000,
-        problems: 50,
-        status: 1,
-        admins: 1
-    });
+// onComponentUpdate()??
+
+// let testComp = [];
+// for (var i = 0; i < 2; i++) {
+//     testComp.push({
+//         id: i,
+//         name: "competition " + i,
+//         desc: "description " + i,
+//         startDate: new Date(),
+//         endDate: new Date(),
+//         duration: 1000,
+//         problems: 50,
+//         status: 1,
+//         admins: 1
+//     });
+// }
+
+class LandingBase extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            competitions: []
+        };
+    }
+
+    componentDidMount() {
+        this.props.firebase
+            .receiveCompetitionUpdates(this.competitionUpdateCallback);
+    }
+
+    competitionUpdateCallback = (result) => {
+        this.setState({ competitions: [] }) // Empty array
+        result.forEach(doc => {
+            this.setState(prevState => ({
+                competitions: [...prevState.competitions, doc.data()] // Append to array
+            }));
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Competitions</h1>
+                {this.state.competitions ? this.state.competitions.map(x => {
+                    return (
+                        <CompetitionCard
+                            id = {x.id}
+                            name = {x.name}
+                            desc = {x.desc}
+                            startDate = {x.startDate}
+                            endDate = {x.endDate}
+                            duration = {x.duration}
+                            problems = {x.problems}
+                            status = {x.status}
+                            admins = {x.admins}
+                        />
+
+                        // <CompetitionProblemCard
+                        //     problem_number={x.problem_number}
+                        //     attempts_made={x.attempts_made}
+                        //     completed={x.completed}
+                        // />
+                    )
+                }) : null}
+            </div>
+        )
+    }
 }
 
-const Landing = () => (
-    <div>
-        <h1>Competitions</h1>
-        {competitions.map(x => {
-            return (
-                <CompetitionCard
-                id = {x.id}
-                name = {x.name}
-                desc = {x.desc}
-                startDate = {x.startDate}
-                endDate = {x.endDate}
-                duration = {x.duration}
-                problems = {x.problems}
-                status = {x.status}
-                admins = {x.admins}
-                />
-
-                // <CompetitionProblemCard
-                //     problem_number={x.problem_number}
-                //     attempts_made={x.attempts_made}
-                //     completed={x.completed}
-                // />
-            )
-        })}
-    </div>
-);
+const Landing = compose(
+    withFirebase
+)(LandingBase);
 
 export default Landing;
